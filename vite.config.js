8ea23ae19aaf535cece2e94596d762055c9228c3
Your Name <decoder@game.com>
2026-03-13 18:59:37 +0530

    fix(deploy): resolve dependencies conflict and set base url for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Blood_Donation/',
   optimizeDeps: {
     include: ['prop-types']
   }
